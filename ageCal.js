f9219005c3155533840855cf29122a32bf187ee6
Emon <157619516+caseiyi@users.noreply.github.com>
2024-10-05 09:49:56 +0600

    Update ageCal.js

--- ageCal.js
+++ ageCal.js
@@ -9,6 +9,10 @@ function calAge(){
     let d2 = today.getDate();
     let m2 =today.getMonth()+1;
     let y2 = today.getFullYear();
+    if(d1>=d2 && m1>=m2 && y1==y2 || y1>y2||(m1>m2 && y1==y2)){
+        document.querySelector(".age").textContent =`You are not old enough to use this feature`;
+    }
+    else{
     let y3,m3=0,d3=0;
     y3 =y2-y1;
     if(m2>=m1){
@@ -23,8 +27,8 @@ function calAge(){
     m3--;
     d3=monthDays(y1,m1) +(d2 -d1);
     }
-    document.querySelector(".age").textContent =`you are ${y3} years ${m3}months ${d3}days old`;
-    console.log(y2,m2,d2);
+    document.querySelector(".age").textContent =`You are ${y3} years ${m3}months ${d3}days old`;
+    console.log(y2,m2,d2);}
 }
 
 function monthDays(year,month) {
@@ -32,28 +36,3 @@ function monthDays(year,month) {
 }
 let btn = document.querySelector("button");
 btn.addEventListener('click',calAge);
-
-const object = {
-    name:"urmd",
-    age:38,
-    job:'jobless'
-}
-let res = Object.keys(object);
-const person = {
-    fname:"John",
-    lname:"Doe",
-    age:25
-  }; 
-  
-  let x;
-  for(x in person) {
-  if(x !=='lname')
- {   console.log(x);
-    let y = person[x];
-    console.log(y);
-    }else{ continue; }
-  }
-
-  let z = person["fname"];
-  let arr = ['djf','samsu'];
-  let c = arr.includes('djf')
